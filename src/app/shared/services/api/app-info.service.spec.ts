9fedca3b063f315f2922b3e5f7bb4c959b9e46ad
Roger Martin <rmartin@aegispremier.com>
2022-01-21 17:54:40 -0700

    Fix unit test

--- src/app/shared/services/api/app-info.service.spec.ts
+++ src/app/shared/services/api/app-info.service.spec.ts
@@ -1,14 +1,17 @@
 import { HttpClient } from '@angular/common/http';
 import { instance, mock } from 'ts-mockito';
+import { AppInfoRepository } from '../../repositories/app-info.repository';
 import { AppInfoService } from './app-info.service';
 
 describe('AppInfoService', () => {
   let service: AppInfoService;
   let httpClientMock: HttpClient;
+  let appInfoRepositoryMock: AppInfoRepository;
 
   beforeEach(() => {
     httpClientMock = mock(HttpClient);
-    service = new AppInfoService(instance(httpClientMock));
+    appInfoRepositoryMock = mock(AppInfoRepository);
+    service = new AppInfoService(instance(httpClientMock), instance(appInfoRepositoryMock));
   });
 
   it('should create the service', () => {
